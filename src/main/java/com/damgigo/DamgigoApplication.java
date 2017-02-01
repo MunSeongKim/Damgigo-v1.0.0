ab2d983804953419a40d41462ddbed37d7d67e9c
MunSeongKim <chyin370@naver.com>
2017-02-02 00:37:10 +0900

    new created
    initialize

---
+++ src/main/java/com/damgigo/DamgigoApplication.java
@@ -0,0 +1,12 @@
+package com.damgigo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DamgigoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DamgigoApplication.class, args);
+	}
+}
