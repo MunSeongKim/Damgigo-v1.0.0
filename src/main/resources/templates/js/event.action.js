13c03e0bf969019692b769b160dd6dead7229749
MunSeongKim <chyin370@naver.com>
2017-02-07 00:02:16 +0900

    17.02.06 메뉴조합
    index에서 메뉴페이지 로딩

--- src/main/resources/templates/js/event.action.js
+++ src/main/resources/templates/js/event.action.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
-	$('#nav').load("/index/nav.html");
+	$('#nav').load("/web/index/nav.html");
+	$('#body').load("/web/users/usertest.html");
 	$('#check_student').show();
 	$('#check_teacher').hide();
 	$("#form_n_snslink").show();
