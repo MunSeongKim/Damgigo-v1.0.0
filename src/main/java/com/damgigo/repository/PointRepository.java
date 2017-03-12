bbdca0442e34f8e131b1ebf98bbb4ebf36fe7edc
MunSeongKim <chyin370@naver.com>
2017-03-12 21:38:56 +0900

    AOP 기본 셋팅 완료
    Advice 생성 진행하기

--- src/main/java/com/damgigo/repository/PointRepository.java
+++ src/main/java/com/damgigo/repository/PointRepository.java
@@ -1,9 +1,13 @@
 package com.damgigo.repository;
 
+import java.util.Map;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.damgigo.domain.User;
 
 public interface PointRepository extends JpaRepository<User, Integer> {
+
+	void save(Map<String, Object> paramMap);
 
 }
