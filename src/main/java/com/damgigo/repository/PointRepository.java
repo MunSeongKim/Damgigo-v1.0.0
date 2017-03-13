20b97c2cd4b7e7533348799a62b349779db54639
MunSeongKim <chyin370@naver.com>
2017-03-13 22:37:32 +0900

    Logger 설정, AOP 샘플 Advice 및 컨트롤러 작성 후 테스트 완료

--- src/main/java/com/damgigo/repository/PointRepository.java
+++ src/main/java/com/damgigo/repository/PointRepository.java
@@ -7,7 +7,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.damgigo.domain.User;
 
 public interface PointRepository extends JpaRepository<User, Integer> {
-
 	void save(Map<String, Object> paramMap);
-
 }
